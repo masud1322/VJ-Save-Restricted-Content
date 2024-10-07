29fd5fb39fbceaa1d12cbab0bf765dd049e0a65e
masud1322 <87791667+masud1322@users.noreply.github.com>
2024-10-07 23:43:07 +0600

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "17462098"))
 API_HASH = os.environ.get("API_HASH", "149b3719dc136ddd05624dc69190dffd")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://realtechbd3:OMZVjlDOUiR8ODfO@cluster0.cr9m6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://lelej78399:8uH6Z0GYhX9sizXp@cluster0.zymda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
